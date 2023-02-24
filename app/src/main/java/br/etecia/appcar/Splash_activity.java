88da0a94407d15a1d5f8c2be7c414963708efe46
MedeirosGS <gabriel.medeiros57@etec.sp.gov.br>
2023-02-23 22:00:38 -0300

    Janela de Login

---
+++ app/src/main/java/br/etecia/appcar/Splash_activity.java
@@ -0,0 +1,14 @@
+package br.etecia.appcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Splash_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
